27bb438fb1135e465b251260ec6ccabb89bf2df7
Alfredomty <angelgranocruz@gmail.com>
2024-11-14 21:31:34 -0800

    compute_prob done

--- hmm.py
+++ hmm.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 class HMM:
     def __init__(self, a, b, c, d, f, evidence):
+
         # X_t-1 True: b true, 1-b false 
         # X_t-1 False: c true, 1-c false
         self.transition_matrix = np.array([[b, 1 - b], [c, 1 - c]]) 
@@ -12,10 +13,36 @@ class HMM:
 
         # P(X0) = a true, 1-a false
         self.initial_state = np.array([a, 1 - a])  
+
+        # Binary evidence
         self.evidence = evidence
 
     def compute_prob(self):
-        pass
+
+        # Setting initial state
+        state_probs = self.initial_state
+
+        for e in self.evidence:
+            # Prediction step: <Px,-Px>
+            predicted_state_probs = np.dot(state_probs, self.transition_matrix)
+
+            # Update step based on current evidence
+            if e == 't':
+                obs_probs = self.observation_matrix[:, 0]  # Observation likelihood for E=true
+            elif e == 'f':
+                obs_probs = self.observation_matrix[:, 1]  # Observation likelihood for E=false
+            else:
+                raise ValueError(f"Invalid evidence value: {e}") # In case there is something that isnt t or f
+
+            # Element-wise multiplication for the update step
+            updated_state_probs = predicted_state_probs * obs_probs
+
+            # Normalization
+            normalization_factor = np.sum(updated_state_probs)
+            state_probs = updated_state_probs / normalization_factor
+
+        return tuple(state_probs)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
