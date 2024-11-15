01cac08813ee8c76066f6ef338879ef220d5926e
Alfredomty <angelgranocruz@gmail.com>
2024-11-14 21:14:35 -0800

    added input file reading

--- hmm.py
+++ hmm.py
@@ -1,7 +1,9 @@
 import sys
 class HMM:
     pass
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python hmm.py <input_file>")
         sys.exit(1)
+    input_file = sys.argv[1]
